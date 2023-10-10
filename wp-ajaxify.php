2c65b1fad50646214f1234d5e8ef9bfa6e092ad0
vijayhardaha <vijayhardaha@gmail.com>
2023-10-11 00:11:46 +0530

    chore(test): run lint test

--- wp-ajaxify.php
+++ wp-ajaxify.php
@@ -17,7 +17,7 @@
  * @package WP_Ajaxify
  */
 
-defined( 'ABSPATH' ) || die( 'Don\'t run this file directly!' );
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
 if ( ! class_exists( 'WP_Ajaxify' ) ) {
 
